acc1944cd9b9f636a809ab700ab5aae5be4bebc8
elena <elena.dobranitsa@entromy.com>
2024-11-18 02:32:23 +0200

    Initial commit with tests and project setup

---
+++ hardhat.config.cjs
@@ -0,0 +1,11 @@
+// require("@nomicfoundation/hardhat-toolbox");
+// require("solidity-coverage");
+// require("@nomicfoundation/hardhat-chai-matchers");
+import "@nomicfoundation/hardhat-toolbox";
+import "solidity-coverage";
+import "@nomicfoundation/hardhat-chai-matchers";
+
+
+module.exports = {
+  solidity: "0.8.13",
+};
